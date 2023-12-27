c7fd6a5af5f881e0e3a4aa784b77355d62725e6d
hakan <tasbeyhakan@gmail.com>
2023-12-27 22:56:47 +0300

    comp. add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'React Movie App', href: '#', current: true }
 ]
 
 function classNames(...classes) {
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const [movie, setMovie] = useState([]);
 
-export default Main
+  const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
+  const getMovies = async () => {
+    const { data } = await axios(url);
+    setMovie(data.results);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {movie.map((item) => (
+        <MovieCard key={item.id} {...item} />
+      ))}
+    </div>
+  );
+};
+
+export default Main;
